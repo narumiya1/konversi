67fcc0d9b9f64667135ad9e54c9daba270017924
narumiya1 <Manunited22>
2020-07-16 15:51:47 +0700

    update Reamur

--- app/src/main/java/com/eijun/konversi/MainActivity.java
+++ app/src/main/java/com/eijun/konversi/MainActivity.java
@@ -19,17 +19,17 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView textViewHasil ;
-    private AppCompatEditText editText_temp1, editText_temp2 ;
-    private LinearLayout linearLayout_hasil ;
+    private TextView textViewHasil;
+    private AppCompatEditText editText_temp1, editText_temp2;
+    private LinearLayout linearLayout_hasil;
     private String[] temperatures = new String[]{
             "\u00B0C",
             "\u00B0R",
             "\u00B0F",
             "K"
-    } ;
+    };
 
-    private Temperatur temperatur ;
+    private Temperatur temperatur;
 
 
     @Override
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         temperatur = new Temperatur(this);
 
-        linearLayout_hasil = findViewById(R.id.layout_hasil) ;
-        textViewHasil = findViewById(R.id.tv_hasil) ;
+        linearLayout_hasil = findViewById(R.id.layout_hasil);
+        textViewHasil = findViewById(R.id.tv_hasil);
 
         setUpToolbar();
 
@@ -51,8 +51,6 @@ public class MainActivity extends AppCompatActivity {
         editText_temp2.setHint(SharedPrefTemp.getTempSymbol2(MainActivity.this));
         editText_temp2.setKeyListener(null);
         editText_temp2.setClickable(false);
-
-
 
 
         //spinner y adapter
@@ -68,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                String temp_symbol1 = temperatures[position] ;
-                SharedPrefTemp.setTemperature1(MainActivity.this, temp_symbol1, position );
+                String temp_symbol1 = temperatures[position];
+                SharedPrefTemp.setTemperature1(MainActivity.this, temp_symbol1, position);
                 editText_temp1.setHint(temperatures[position]);
             }
 
@@ -80,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //spinner y adater 2
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, temperatures) ;
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, temperatures);
         arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         AppCompatSpinner appCompatSpinner2 = findViewById(R.id.spinner_2_tempr);
@@ -89,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         appCompatSpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String temp_symbol2 = temperatures[position] ;
+                String temp_symbol2 = temperatures[position];
                 SharedPrefTemp.setTemperature2(MainActivity.this, temp_symbol2, position);
                 editText_temp2.setHint(temperatures[position]);
             }
@@ -100,53 +98,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        AppCompatButton btn_count = findViewById(R.id.btn_count) ;
+        AppCompatButton btn_count = findViewById(R.id.btn_count);
         btn_count.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Objects.requireNonNull(editText_temp1.getText()).toString().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Masukan nilai suhu yang ingin dikonversi", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
 
-                    if (linearLayout_hasil.getVisibility() == View.GONE){
+                    if (linearLayout_hasil.getVisibility() == View.GONE) {
                         linearLayout_hasil.setVisibility(View.VISIBLE);
                     }
 
                     String symbol_temp1 = SharedPrefTemp.getTempSymbol1(MainActivity.this);
-                    String symbol_temp2 = SharedPrefTemp.getTempSymbol2(MainActivity.this) ;
+                    String symbol_temp2 = SharedPrefTemp.getTempSymbol2(MainActivity.this);
 
                     double value_to_convertion = Double.parseDouble(editText_temp1.getText().toString());
 
                     if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("\u00B0R")) {
                         editText_temp2.setText(temperatur.CelciusToReamur(value_to_convertion));
-                        textViewHasil.setText(temperatur.getRumus("\u00B0C","\u00B0R",
+                        textViewHasil.setText(temperatur.getRumus("\u00B0C", "\u00B0R",
                                 value_to_convertion, temperatur.CelciusToReamur(value_to_convertion)));
 
-                    }else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("\u00B0F")) {
+                    } else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("\u00B0F")) {
                         editText_temp2.setText(temperatur.CeliusToFahrenheit(value_to_convertion));
-                        textViewHasil.setText(temperatur.getRumus("\u00B0C","\u00B0F",
-                                value_to_convertion, temperatur.CeliusToFahrenheit(value_to_convertion) ));
+                        textViewHasil.setText(temperatur.getRumus("\u00B0C", "\u00B0F",
+                                value_to_convertion, temperatur.CeliusToFahrenheit(value_to_convertion)));
 
-                    }
-                    else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("K")) {
+                    } else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("K")) {
                         editText_temp2.setText(temperatur.CelciusToKelvn(value_to_convertion));
-                        textViewHasil.setText(temperatur.getRumus("\u00B0C","K",
-                                value_to_convertion, temperatur.CelciusToKelvn(value_to_convertion) ));
+                        textViewHasil.setText(temperatur.getRumus("\u00B0C", "K",
+                                value_to_convertion, temperatur.CelciusToKelvn(value_to_convertion)));
 
                     }
 
+                    // Reamur
 
-                    else if (symbol_temp1.equals("\u00B0R") && symbol_temp2.equals("\u00B0C") ) {
-                        editText_temp2.setText(temperatur.CelciusToReamur(value_to_convertion));
+                    else if (symbol_temp1.equals("\u00B0R") && symbol_temp2.equals("\u00B0C")) {
+                        editText_temp2.setText(temperatur.ReamurToCelcius(value_to_convertion));
                         textViewHasil.setText(temperatur.getRumus("\u00B0R", "\u00B0C",
                                 value_to_convertion, temperatur.ReamurToCelcius(value_to_convertion)));
-                    }
-
-                    else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("\u00B0C")) {
+                    } else if (symbol_temp1.equals("\u00B0R") && symbol_temp2.equals("\u00B0F")) {
+                        editText_temp2.setText(temperatur.ReamurToFahrenheit(value_to_convertion));
+                        textViewHasil.setText(temperatur.getRumus("\u00B0R", "\u00B0F",
+                                value_to_convertion, temperatur.ReamurToFahrenheit(value_to_convertion)));
+                    } else if (symbol_temp1.equals("\u00B0R") && symbol_temp2.equals("K")) {
+                        editText_temp2.setText(temperatur.ReamurToKelvin(value_to_convertion));
+                        textViewHasil.setText(temperatur.getRumus("\u00B0R", "K",
+                                value_to_convertion, temperatur.ReamurToKelvin(value_to_convertion)));
+                    } else if (symbol_temp1.equals("\u00B0C") && symbol_temp2.equals("\u00B0C")) {
                         editText_temp2.setText(temperatur.check_after_decimal_point(value_to_convertion));
                         textViewHasil.setText("\u00B0C  =  " + temperatur.check_after_decimal_point(value_to_convertion));
-                    }
-                    else if (symbol_temp1.equals("\u00B0F") && symbol_temp2.equals("\u00B0F")) {
+                    } else if (symbol_temp1.equals("\u00B0R") && symbol_temp2.equals("\u00B0R")) {
+                        editText_temp2.setText(temperatur.check_after_decimal_point(value_to_convertion));
+                        textViewHasil.setText(" \u00B0R = " + temperatur.check_after_decimal_point(value_to_convertion));
+
+                    } else if (symbol_temp1.equals("\u00B0F") && symbol_temp2.equals("\u00B0F")) {
                         editText_temp2.setText(temperatur.check_after_decimal_point(value_to_convertion));
                         textViewHasil.setText("\u00B0F  =  " + temperatur.check_after_decimal_point(value_to_convertion));
                     }
--- app/src/main/java/com/eijun/konversi/Temperatur.java
+++ app/src/main/java/com/eijun/konversi/Temperatur.java
@@ -3,75 +3,98 @@ package com.eijun.konversi;
 import android.content.Context;
 
 public class Temperatur {
-    private Context context ;
+    private Context context;
 
     public Temperatur(Context context) {
         this.context = context;
     }
 
     /**
-     *
      * rumus celcius
      */
 
     public String CelciusToReamur(double celcius) {
         // R = (4/5) x C or R = C x 0,8
-        double R = (4.0/5.0) * celcius ;
-        return check_after_decimal_point(R) ;
+        double R = (4.0 / 5.0) * celcius;
+        return check_after_decimal_point(R);
     }
 
     public String CeliusToFahrenheit(double celcius) {
-        double F = (9.0/5.0) * celcius + 32 ;
-        return check_after_decimal_point(F) ;
+        double F = (9.0 / 5.0) * celcius + 32;
+        return check_after_decimal_point(F);
     }
 
     public String CelciusToKelvn(double celcius) {
-        double K = celcius + 273.15 ;
-        return  check_after_decimal_point(K);
+        double K = celcius + 273.15;
+        return check_after_decimal_point(K);
     }
+
+
+    /**
+     * rumus celcius
+     */
 
     public String ReamurToCelcius(double reamur) {
-        double C = reamur * 0.8  ;
-        return check_after_decimal_point(C) ;
+        double C = reamur / 0.8;
+        return check_after_decimal_point(C);
     }
 
+    public String ReamurToFahrenheit(double reamur) {
+        double F = reamur * 2.25 + 32;
+        return check_after_decimal_point(F);
+    }
+
+    public String ReamurToKelvin(double reamur) {
+        double K = reamur / 0.8 + 273.15;
+        return check_after_decimal_point(K);
+    }
+
+    
     public String check_after_decimal_point(double decimal) {
 
-        String result = null ;
+        String result = null;
 
-        String[] after_point =  String.valueOf(decimal).split("[:.]") ;
+        String[] after_point = String.valueOf(decimal).split("[:.]");
 
-        if (after_point[after_point.length-1].equals("0")) {
-            result =  after_point[0];
-        }else {
-            result = String.valueOf(decimal).replace(".",",") ;
+        if (after_point[after_point.length - 1].equals("0")) {
+            result = after_point[0];
+        } else {
+            result = String.valueOf(decimal).replace(".", ",");
         }
 
-        return result ;
+        return result;
 
     }
 
     public String getRumus(String symbol1, String symbol2, double value_to_coversion, String result) {
 
-        String results = null ;
-        //C to R
-        if ( symbol1.equals("\u00B0C") && symbol2.equals("\u00B0R")) {
-            results = symbol2 + " = " +symbol1+ " x 0,8 \n" + symbol2 +
-                    " = " +check_after_decimal_point(value_to_coversion) +" x 0,8\n " +symbol2+ " = " +result;
+        String results = null;
+        //Celsius
+        if (symbol1.equals("\u00B0C") && symbol2.equals("\u00B0R")) {
+            results = symbol2 + " = " + symbol1 + " x 0,8 \n" + symbol2 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " x 0,8\n " + symbol2 + " = " + result;
+        } else if (symbol1.equals("\u00B0C") && symbol2.equals("\u00B0F")) {
+            results = symbol2 + " = " + symbol1 + " x 9/5 \n" + symbol2 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " x 9/5\n" + symbol2 + " = " + result;
+        } else if (symbol1.equals("\u00B0C") && symbol2.equals("K")) {
+            results = symbol2 + " = " + symbol1 + " + 273,5 \n " + symbol1 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " +273,5 " + symbol2 + " = " + result;
         }
-        //C -> F
-        else if(symbol1.equals("\u00B0C") && symbol2.equals("\u00B0F")) {
-            results = symbol2 + " = " +symbol1+ " x 9/5 \n" + symbol2 +
-                    " = " +check_after_decimal_point(value_to_coversion) + " x 9/5\n" +symbol2+ " = " +result ;
-        }else if(symbol1.equals("\u00B0C") && symbol2.equals("K")) {
-            results = symbol2 + " = " +symbol1+ " + 273,5 \n " +symbol1 +
-                    " = " +check_after_decimal_point(value_to_coversion) + " +273,5 " +symbol2+ " = " +result ;
-        }else if (symbol1.equals("\u00B0R") && symbol2.equals("\u00B0C")) {
-            results = symbol2 + " = " +symbol1+ " * 0,8 \n" +symbol2 +
-                    " = " +check_after_decimal_point(value_to_coversion) + " * 0,8 " +symbol2+ " = " +result;
+        //Reamur
+        else if (symbol1.equals("\u00B0R") && symbol2.equals("\u00B0C")) {
+            results = symbol2 + " = " + symbol1 + " * 0,8 \n" + symbol2 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " * 0,8 " + symbol2 + " = " + result;
+        } else if (symbol1.equals("\u00B0R") && symbol2.equals("\u00B0F")) {
+            results = symbol2 + " = " + symbol1 + " * 2,25 + 32 \n" + symbol2 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " *2,25 + 32" + symbol2 + " \n \t\t  = " + result;
+
+        } else if (symbol1.equals("\u00B0R") && symbol2.equals("K")) {
+            results = symbol2 + " = " + symbol1 + " / 0,8 + 273,15 \n " + symbol2 +
+                    " = " + check_after_decimal_point(value_to_coversion) + " / 0,8 + 273,15 \n \t\t = " + result;
+
         }
 
-        return results ;
+        return results;
     }
 
 }
